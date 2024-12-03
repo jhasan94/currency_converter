0067c4e7b94b66ca12a8f6bc4df9850ed3c27608
jahidhasanm1 <jahidnsu142@gmail.com>
2024-12-03 11:09:26 +0300

    change base url and endpoint

--- lib/core/app_constant/api_end_points.dart
+++ lib/core/app_constant/api_end_points.dart
@@ -1,6 +1,9 @@
 enum RequestType { get, post }
 
 class ApiEndPoints {
-  static String baseUrl = 'https://v6.exchangerate-api.com';
-  static String currency = '/v6/6bf723403158653eeed70229/latest/usd';
+  //static String baseUrl = 'https://v6.exchangerate-api.com';
+  //static String currency = '/v6/6bf723403158653eeed70229/latest/usd';
+  static String baseUrl = 'https://free.currconv.com';
+  static String apiKey = '790ab2007cdf7c3989e5';
+  static String currencies = "/api/v8/countries?apiKey=$apiKey";
 }
